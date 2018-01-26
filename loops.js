ef76a84c557b019d02a7ca752d2b6f562c00f023
Josue Rios <josuerd89@gmail.com>
2018-01-26 01:08:44 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array) {
+var array=[]
+for (let x=1; x<25; x++){
+  if (x===1) {
+    array.push= ( "I am 1 strange loop" )
+  }
+  else {
+    array.push=("I am " + x + " strange loops.")
+  }
+} return array
+}
